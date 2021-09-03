1d109f59296a72a39dc3977efcc1a533b31e9d61
ivan776 <mendozaivan1224@gmail.com>
2021-09-03 02:33:41 -0700

    Created User Login

---
+++ app/src/main/java/com/example/wk01hw02solo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.wk01hw02solo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText userName;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
